47bc3e80cdf241421efb255990ff4400621ee941
Sonia Sun <soniasun0925@gmail.com>
2022-09-25 14:22:30 -0700

    Add Router, (react-router-dom).

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,13 @@
 
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand} from 'reactstrap';
 import '../App.css';
 import { DISHES } from '../shared/dishes';
-import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Menu2 from './MenuComponent2';
+import Header from './HeaderComponent';
+import Home from './HomeComponent';
+import Footer from './FooterComponent';
+import { Routes, Route, Navigate } from "react-router-dom";
 
 
 class Main extends React.Component {
@@ -15,35 +17,27 @@ class Main extends React.Component {
 
     this.state = { 
         dishes: DISHES,
-        selectedDish: 0
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
-  // isToDisplay(){
-  //   const dish = this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]
-  //   if (dish != null){
-  //     return dish
-  //   } else {
-  //     return this.state.dishes[0]
-  //   }
-  // }
 
   
-  render(){
+  render() {
+    const HomePage = () => {
+      return(
+          <Home />
+      );
+    }
+    
     return (
       <div>
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Pastoral Restaurant</NavbarBrand>
-            </div>
-          </Navbar>
-          <Menu2 dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-          {/* <Dishdetail dish={this.state.dishes[0]}/> */}
-          <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+          <Header/>
+          <Routes>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu2 dishes={this.state.dishes} />} />
+              <Navigate to="/home" />
+          </Routes>
+          <Footer/>
         </div>
     );
 }
--- src/components/MenuComponent2.js
+++ src/components/MenuComponent2.js
@@ -35,4 +35,5 @@ const Menu2 = (props) => {
     )
 
 }
+
 export default Menu2;
